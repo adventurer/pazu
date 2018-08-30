b793b48defc6c21f2840db232fb0d4546e085fa9
Gogs <gogs@fake.local>
2018-08-30 09:46:26 +0800

    1

--- health/healthcheck.go
+++ health/healthcheck.go
@@ -34,7 +34,7 @@ func waringCheck(id int, err string) {
 		}
 		if v.Report <= 0 {
 			log.Println("waring send a mail:", v.Name)
-			mail.SendEmail(mail.NewEmail("16620808100@163.com", "健康检查故障", v.Name+"发生故障:"+err, "html"))
+			mail.SendEmail(mail.NewEmail("16620808100@163.com", "健康检查故障", v.Name+"发生故障:"+err, "text"))
 			waring[k].Report = cache.MemHealthTable[k].Report
 		}
 	}
@@ -69,6 +69,9 @@ func send(v models.Health) {
 		waringCheck(v.Id, fmt.Sprintf("err:%s,status:%s", err, "cant access"))
 		responseJson = CheckResult{Health: v, Code: -1, Cost: (end - begin) / 1000000, Msg: fmt.Sprintf("%s", err)}
 	} else {
+		if health.Response.StatusCode != 200 {
+			waringCheck(v.Id, fmt.Sprintf("err:%s,status:%s", "", health.Response.Status))
+		}
 		responseJson = CheckResult{Health: v, Code: health.Response.StatusCode, Cost: (end - begin) / 1000000, Msg: fmt.Sprintf("%s", health.Response.Status)}
 	}
 
